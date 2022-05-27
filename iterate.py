5917121ec81439523c9f9943f78744b20937e971
MaywellPanda <91027017+MaywellPanda@users.noreply.github.com>
2022-05-27 12:56:21 +0100

    Update iterate.py

--- iterate.py
+++ iterate.py
@@ -1,4 +1,11 @@
-#For this code i allowed X to = 1 and A = 1
+#This was my first time coding in python upon closer inspection of the question i felt that this code i wrote didn't meet the requirements.
+#i have included a second bit of code NOT WRITTEN BY ME, that i feel would lead to the correct solution ( allthough the question was confusing to me)
+
+#question:In a language of your choice, using all best practice principles you’re aware of, could you please provide code that 
+#iterates in multiples of A until X, then in multiples of A + 1 until 2X, then multiples of A + 2 until 3X. Please state any assumptions you’ve made
+
+#For this code i allowed X to = 0 and A = 1
+
 
 iterable = [1,2,3]
 for item in iterable:
