b1d7926a23e0630d14825ecac36d10f755d6c3e6
MaywellPanda <91027017+MaywellPanda@users.noreply.github.com>
2022-05-27 12:49:51 +0100

    Add files via upload

---
+++ Not my code.py
@@ -0,0 +1,14 @@
+# This is not my code but i feel that if i had better knowledge this would have led me to the proper solution. (found on stackoverflow)
+def multiple(x, a):
+  
+    # inserts all elements from n to 
+    # (m * n)+1 incremented by n.
+    a = range(n, (m * n)+1,n)
+    print(*a)
+  
+# driver code 
+m = 4
+n = 3
+multiple(m, n)
+
+
---
+++ iterate.py
@@ -0,0 +1,7 @@
+#For this code i allowed X to = 1 and A = 1
+
+iterable = [1,2,3]
+for item in iterable:
+
+ print(item)
+   
